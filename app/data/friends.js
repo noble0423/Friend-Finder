22f682d16d880a308bf2b38bee5b96a218af5cde
noble0423 <ryan.noble82@gmail.com>
2018-03-22 08:06:15 -0700

    added 10 total 'friend match' profiles to friendsArray on friends.js. Added new file, surveyQuestions.js that will be used to house survey questions and the scrolling logic to display them (if I have the time, otherwise, questions will be listed on the survey.html page as a form/list. ensured all the links on home.html and survey.html are working.

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,22 +6,78 @@
 
 var friendsArray = [
     {
-        "name" : "Ahmed",
-        "photo" : "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name" : "Stephanie",
+        "photo" : "http://static6.businessinsider.com/image/56abccff58c32386008b6715-1190-625/these-are-the-best-ways-to-improve-your-online-dating-profile-according-to-science.jpg",
         "scores" : [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
+            5, 1, 4, 4, 5, 1, 2, 5, 4, 1
         ]
-    }
+    },
+    {
+        "name" : "Ricky",
+        "photo" : "http://cdn77.sadanduseless.com/wp-content/uploads/2013/11/d5.jpg",
+        "scores" : [
+            3, 2, 5, 4, 5, 1, 2, 5, 4, 1
+        ]
+    },
+    {
+        "name" : "Katie",
+        "photo" : "https://get.pxhere.com/photo/girl-woman-hair-color-brown-hairstyle-long-hair-close-up-face-sunglasses-eye-glasses-head-eyewear-funny-humour-faceless-anonymous-hairs-organ-joke-brown-hair-fashion-accessory-vision-care-joking-anonymously-960513.jpg",
+        "scores" : [
+            3, 1, 2, 4, 1, 5, 4, 5, 3, 3
+        ]
+    },
+    {
+        "name" : "Chris",
+        "photo" : "http://2.bp.blogspot.com/-jjXmfX1n3cg/TqCA0h8S4DI/AAAAAAAABWo/t_KeC9Gjf4Q/s640/Funny+I+am+Probably+Unemployable%2521.jpg",
+        "scores" : [
+            1, 3, 2, 1, 3, 5, 3, 3, 3, 4
+        ]
+    },
+    {
+        "name" : "Meghan",
+        "photo" : "https://s-media-cache-ak0.pinimg.com/736x/7b/ca/f2/7bcaf231019488eb31bae06738029c31.jpg",
+        "scores" : [
+            2, 2, 5, 4, 2, 1, 2, 4, 1, 1
+        ]
+    },
+    {
+        "name" : "Andrew",
+        "photo" : "https://s-media-cache-ak0.pinimg.com/736x/ea/a6/14/eaa614d12b08249db17e6e2ab7c041b8.jpg",
+        "scores" : [
+            4, 4, 2, 2, 3, 1, 2, 2, 4, 5
+        ]
+    },
+    {
+        "name" : "Elizabeth",
+        "photo" : "http://acidcow.com/pics/20131230/russian_social_networks_28.jpg",
+        "scores" : [
+            4, 1, 2, 4, 1, 5, 5, 3, 3, 4
+        ]
+    },
+    {
+        "name" : "Bobby",
+        "photo" : "http://4.bp.blogspot.com/_Ep3rxquKOn0/TVA-yDZvKgI/AAAAAAAAACE/PDDTuAwTLOk/s1600/funny-facebook-fail-guy.jpg",
+        "scores" : [
+            3, 3, 1, 5, 4, 2, 3, 4, 2, 3
+        ]
+    },
+    {
+        "name" : "Melissa",
+        "photo" : "https://pbs.twimg.com/profile_images/1585792155/ugly_400x400.jpg",
+        "scores" : [
+            1, 4, 3, 2, 1, 2, 3, 4, 5, 4
+        ]
+    },
+    {
+        "name" : "Dave",
+        "photo" : "http://www.walesoncraic.com/wp-content/uploads/2015/01/funny-man.jpg",
+        "scores" : [
+            5, 4, 3, 2, 1, 2, 3, 4, 5, 4
+        ]
+    },
 ];
+
+
 
 // Export the array to make it accessible to other files using require.
 module.exports = friendsArray;
---
+++ app/data/surveyQuestions.js
@@ -0,0 +1,1 @@
+// If time, take questions out of out the survey.html page as a list, create question array and have this page loop through ea question only moving onto the next question either after a question has been answered, or give a 15 sec time limit and if not answered, score the question as a 1?
