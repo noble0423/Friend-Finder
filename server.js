d81bc1b2d8e9fb9df2ce5e09bb90490cc8cf4971
noble0423 <ryan.noble82@gmail.com>
2018-03-21 23:13:46 -0700

    initial fdirectory of folders and files created. .DS_Store and node_modules added to .gitignore. express and body-parser mods have been installed and are listed on package.json file. initial setup code for those mods (as well as path mod) have been added and saved to server.js.

---
+++ server.js
@@ -0,0 +1,36 @@
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// We are requiring ("linking") the following route pages...we incl our api-route first, b/c that is where we are pulling our data to display inside out html pages
+require("./app/routing/api-routes.js")(app);
+require("./app/routing/html-routes.js")(app);
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
