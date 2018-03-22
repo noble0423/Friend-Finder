1a4d5935f2bbb4e606cc9185f7d058029fc3527a
noble0423 <ryan.noble82@gmail.com>
2018-03-22 09:57:30 -0700

    changed existing database to wrestlers.

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,71 +6,71 @@
 
 var friendsArray = [
     {
-        "name" : "Stephanie",
-        "photo" : "http://static6.businessinsider.com/image/56abccff58c32386008b6715-1190-625/these-are-the-best-ways-to-improve-your-online-dating-profile-according-to-science.jpg",
+        "name" : "Shark Boy",
+        "photo" : "http://img3.rnkr-static.com/user_node_img/101/2013941/870/shark-boy-photo-u2.jpg",
         "scores" : [
             5, 1, 4, 4, 5, 1, 2, 5, 4, 1
         ]
     },
     {
-        "name" : "Ricky",
-        "photo" : "http://cdn77.sadanduseless.com/wp-content/uploads/2013/11/d5.jpg",
+        "name" : "Max Moon",
+        "photo" : "http://static.sportskeeda.com/wp-content/uploads/2016/04/max_moon_lol-1461002627-800.jpg",
         "scores" : [
             3, 2, 5, 4, 5, 1, 2, 5, 4, 1
         ]
     },
     {
-        "name" : "Katie",
-        "photo" : "https://get.pxhere.com/photo/girl-woman-hair-color-brown-hairstyle-long-hair-close-up-face-sunglasses-eye-glasses-head-eyewear-funny-humour-faceless-anonymous-hairs-organ-joke-brown-hair-fashion-accessory-vision-care-joking-anonymously-960513.jpg",
+        "name" : "Tom Spiderman",
+        "photo" : "http://pic.hqshuaimi.com/uploadimg/2013/0923/20130923011606300.jpg",
         "scores" : [
             3, 1, 2, 4, 1, 5, 4, 5, 3, 3
         ]
     },
     {
-        "name" : "Chris",
-        "photo" : "http://2.bp.blogspot.com/-jjXmfX1n3cg/TqCA0h8S4DI/AAAAAAAABWo/t_KeC9Gjf4Q/s640/Funny+I+am+Probably+Unemployable%2521.jpg",
+        "name" : "Salvatore Sincere",
+        "photo" : "https://vignette4.wikia.nocookie.net/wrestlepedia/images/e/e7/Salvatore_Sincere.jpg/revision/latest?cb=20161116234119",
         "scores" : [
             1, 3, 2, 1, 3, 5, 3, 3, 3, 4
         ]
     },
     {
-        "name" : "Meghan",
-        "photo" : "https://s-media-cache-ak0.pinimg.com/736x/7b/ca/f2/7bcaf231019488eb31bae06738029c31.jpg",
+        "name" : "Mordecai",
+        "photo" : "http://img.bleacherreport.net/img/slides/photos/002/383/982/mordecai_crop_north.png?w=630&h=420&q=75",
         "scores" : [
             2, 2, 5, 4, 2, 1, 2, 4, 1, 1
         ]
     },
     {
-        "name" : "Andrew",
-        "photo" : "https://s-media-cache-ak0.pinimg.com/736x/ea/a6/14/eaa614d12b08249db17e6e2ab7c041b8.jpg",
+        "name" : "Koko B. Ware",
+        "photo" : "http://www.onlineworldofwrestling.com/wp-content/uploads/2016/03/48_Koko-001.jpg",
         "scores" : [
             4, 4, 2, 2, 3, 1, 2, 2, 4, 5
         ]
     },
     {
-        "name" : "Elizabeth",
-        "photo" : "http://acidcow.com/pics/20131230/russian_social_networks_28.jpg",
+        "name" : "The Shockmaster",
+        "photo" : "http://capricorncity.com/wp-content/uploads/2013/09/The-Shockmaster.jpg",
         "scores" : [
             4, 1, 2, 4, 1, 5, 5, 3, 3, 4
         ]
     },
     {
-        "name" : "Bobby",
-        "photo" : "http://4.bp.blogspot.com/_Ep3rxquKOn0/TVA-yDZvKgI/AAAAAAAAACE/PDDTuAwTLOk/s1600/funny-facebook-fail-guy.jpg",
+        "name" : "Manataur",
+        "photo" : "https://ringthedamnbell.files.wordpress.com/2015/11/cndxagruyaekwge.jpg",
         "scores" : [
             3, 3, 1, 5, 4, 2, 3, 4, 2, 3
         ]
     },
     {
-        "name" : "Melissa",
-        "photo" : "https://pbs.twimg.com/profile_images/1585792155/ugly_400x400.jpg",
+        "name" : "Bastion Booger",
+        "photo" : "https://ringthedamnbell.files.wordpress.com/2015/01/bastion-booger.png",
         "scores" : [
             1, 4, 3, 2, 1, 2, 3, 4, 5, 4
         ]
     },
     {
-        "name" : "Dave",
-        "photo" : "http://www.walesoncraic.com/wp-content/uploads/2015/01/funny-man.jpg",
+        "name" : "Knucleball",
+        "photo" : "http://i1.ytimg.com/vi/U4MTEmYFRoU/hqdefault.jpg",
         "scores" : [
             5, 4, 3, 2, 1, 2, 3, 4, 5, 4
         ]
