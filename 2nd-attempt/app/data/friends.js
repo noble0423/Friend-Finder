7148847d5f7cf9ce8d7f62d552f1a0862c09f50b
noble0423 <ryan.noble82@gmail.com>
2018-03-24 16:13:26 -0700

    moved all orig files for HW into 1st attempt folder in order to rebuild the app from scratch in the 2nd attempt folder. setup all initial files for 2nd attempt.

---
+++ 2nd-attempt/app/data/friends.js
@@ -0,0 +1,83 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the friends info.
+// Initially we just set it equal to a "dummy" friend/info.
+// ===============================================================================
+
+var partnerArray = [
+    {
+        "name" : "Shark Boy",
+        "photo" : "http://img3.rnkr-static.com/user_node_img/101/2013941/870/shark-boy-photo-u2.jpg",
+        "scores" : [
+            5, 1, 4, 4, 5, 1, 2, 5, 4, 1
+        ]
+    },
+    {
+        "name" : "Max Moon",
+        "photo" : "http://static.sportskeeda.com/wp-content/uploads/2016/04/max_moon_lol-1461002627-800.jpg",
+        "scores" : [
+            3, 2, 5, 4, 5, 1, 2, 5, 4, 1
+        ]
+    },
+    {
+        "name" : "Tom Spiderman",
+        "photo" : "http://pic.hqshuaimi.com/uploadimg/2013/0923/20130923011606300.jpg",
+        "scores" : [
+            3, 1, 2, 4, 1, 5, 4, 5, 3, 3
+        ]
+    },
+    {
+        "name" : "Salvatore Sincere",
+        "photo" : "https://vignette4.wikia.nocookie.net/wrestlepedia/images/e/e7/Salvatore_Sincere.jpg/revision/latest?cb=20161116234119",
+        "scores" : [
+            1, 3, 2, 1, 3, 5, 3, 3, 3, 4
+        ]
+    },
+    {
+        "name" : "Mordecai",
+        "photo" : "http://img.bleacherreport.net/img/slides/photos/002/383/982/mordecai_crop_north.png?w=630&h=420&q=75",
+        "scores" : [
+            2, 2, 5, 4, 2, 1, 2, 4, 1, 1
+        ]
+    },
+    {
+        "name" : "Koko B. Ware",
+        "photo" : "http://www.onlineworldofwrestling.com/wp-content/uploads/2016/03/48_Koko-001.jpg",
+        "scores" : [
+            4, 4, 2, 2, 3, 1, 2, 2, 4, 5
+        ]
+    },
+    {
+        "name" : "The Shockmaster",
+        "photo" : "http://capricorncity.com/wp-content/uploads/2013/09/The-Shockmaster.jpg",
+        "scores" : [
+            4, 1, 2, 4, 1, 5, 5, 3, 3, 4
+        ]
+    },
+    {
+        "name" : "Manataur",
+        "photo" : "https://ringthedamnbell.files.wordpress.com/2015/11/cndxagruyaekwge.jpg",
+        "scores" : [
+            3, 3, 1, 5, 4, 2, 3, 4, 2, 3
+        ]
+    },
+    {
+        "name" : "Bastion Booger",
+        "photo" : "https://ringthedamnbell.files.wordpress.com/2015/01/bastion-booger.png",
+        "scores" : [
+            1, 4, 3, 2, 1, 2, 3, 4, 5, 4
+        ]
+    },
+    {
+        "name" : "Knucleball",
+        "photo" : "http://i1.ytimg.com/vi/U4MTEmYFRoU/hqdefault.jpg",
+        "scores" : [
+            5, 4, 3, 2, 1, 2, 3, 4, 5, 4
+        ]
+    },
+];
+
+
+
+// Export the array to make it accessible to other files using require.
+module.exports = partnerArray;
